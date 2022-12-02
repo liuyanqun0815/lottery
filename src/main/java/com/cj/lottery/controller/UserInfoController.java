1b231fce63592de97020fa4c6bdf37d7cf5479a0
lyq <1163866926@qq.com>
2022-12-02 09:46:30 +0800

    增加打印

--- src/main/java/com/cj/lottery/controller/UserInfoController.java
+++ src/main/java/com/cj/lottery/controller/UserInfoController.java
@@ -107,5 +107,6 @@ public class UserInfoController {
     public static void main(String[] args) {
 
         System.out.println(System.currentTimeMillis());
+        System.out.println(Math.max(1,1111));
     }
 }
